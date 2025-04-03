fdca934c77d8911999d16e0d1ec53f0dc14d6094
Vincentiaaa <vincent.camus@ynov.com>
2025-04-03 17:55:15 +0200

    Graphiques faits , value_counts fait, sum fait, dériv colonnes fait, interface interactive fait, reste plus qu'a finir les prérequis puis styliser

--- app.py
+++ app.py
@@ -10,21 +10,27 @@ del ds3["Critical"]
 ds3[['Physical Damage', 'Magic Damage', 'Fire Damage','Lightning Damage', 'Dark Damage']] = ds3["Damage"].str.split('/', expand=True)
 del ds3["Damage"]
 
-page = st.sidebar.radio("Navigation", ["Accueil","Toutes les armes", "Recherche spécifique"])
+page = st.sidebar.radio("Navigate", ["Home","Weapon list", "Search", "Fun facts"])
 
-if page == "Accueil":
-    st.title("Bienvenue sur DS3 Weapons !")
-    st.image("https://p325k7wa.twic.pics/high/dark-souls/dark-souls-2/00-page-setup/ds2_game-thumbnail.jpg?twic=v1/resize=760/step=10/quality=80", caption = "image dark souls 2")
-    ds3graph = ds3[["Fire Damage", "Magic Damage"]]
-    st.scatter_chart(ds3graph)
+if page == "Home":
+    st.title("Welcome in DS3 Weapons !")
+    if st.button("How Many Weapons are there ?"):
+        much = ds3["Name"]
+        total = much.count()
+        st.write(f"There are {total} Weapons in Dark Souls 3 !")
+        st.image("https://p325k7wa.twic.pics/high/dark-souls/dark-souls-2/00-page-setup/ds2_game-thumbnail.jpg?twic=v1/resize=760/step=10/quality=80", caption = "image dark souls 2")
 
 
-elif page == "Toutes les armes":
-    st.title("Liste des armes de Dark Souls 3 :")
+
+
+
+
+elif page == "Weapon list":
+    st.title("Dark souls 3 weapons list :")
     st.dataframe(ds3)
-elif page == "Recherche spécifique":
-    st.title("Vous cherchez quelque chose ? Regardez nos catégories :")
-    if st.button("10 Meilleures armes de feu"):
+elif page == "Search":
+    st.title("Searching something ? Check out our recommandations :")
+    if st.button("10 Best Fire Weapons"):
         bestfire = ds3.sort_values(by="Fire Damage", ascending=False).iloc[:10]
         st.write(bestfire.loc[:, ["Name", "Fire Damage"]])
         bestfire2 = ds3.sort_values(by="Fire Damage", ascending=True).iloc[-10:]
@@ -35,17 +41,47 @@ elif page == "Recherche spécifique":
         ax.set_ylabel("Item Name", color = "green")
         plt.grid(True)
         st.pyplot(fig)
-    if st.button("10 Meilleures armes physiques"):
+    if st.button("10 Best PHY Weapons"):
         bestphy = ds3.sort_values(by="Physical Damage", ascending=False).iloc[:10]
         st.write(bestphy.loc[:, ["Name", "Physical Damage"]])
-    if st.button("10 Meilleures armes magiques"):
+        bestphy2 = ds3.sort_values(by="Physical Damage", ascending=True).iloc[-10:]
+        fig, ax = plt.subplots()
+        plt.title("10 Best Physical Damage Weapons in Dark Souls 3", color = "blue")
+        ax.plot(bestphy2["Physical Damage"], bestphy2["Name"], marker ='D', linestyle = ':', color = "black")
+        ax.set_xlabel("PHY Damage", color = "brown")
+        ax.set_ylabel("Item Name", color = "green")
+        plt.grid(True)
+        st.pyplot(fig)
+    if st.button("10 Best Magic Weapons"):
         bestmag = ds3.sort_values(by="Magic Damage", ascending=False).iloc[:10]
         st.write(bestmag.loc[:, ["Name", "Magic Damage"]])
-    if st.button("10 Meilleures armes électiques"):
-        bestlig = ds3.sort_values(by="Lightning Damage", ascending=False).iloc[:10]
+        bestmag2 = ds3.sort_values(by="Magic Damage", ascending=True).iloc[-10:]
+        fig, ax = plt.subplots()
+        plt.title("10 Best Magic Damage Weapons in Dark Souls 3", color = "blue")
+        ax.plot(bestmag2["Magic Damage"], bestmag2["Name"], marker ='D', linestyle = ':', color = "black")
+        ax.set_xlabel("Magic Damage", color = "blue")
+        ax.set_ylabel("Item Name", color = "green")
+        plt.grid(True)
+        st.pyplot(fig)
+    if st.button("5 Best Lightning Weapons"):
+        bestlig = ds3.sort_values(by="Lightning Damage", ascending=False).iloc[:5]
         st.write(bestlig.loc[:, ["Name", "Lightning Damage"]])
-    if st.button("10 Meilleures armes occultes"):
-        bestdark = ds3.sort_values(by="Dark Damage", ascending=False).iloc[:10]
-        st.write(bestdark.loc[:, ["Name", "Dark Damage"]])
+        bestlig2 = ds3.sort_values(by="Lightning Damage", ascending=True).iloc[-5:]
+        fig, ax = plt.subplots()
+        plt.title("5 Best Lightning Damage Weapons in Dark Souls 3", color = "blue")
+        ax.plot(bestlig2["Lightning Damage"], bestlig2["Name"], marker ='D', linestyle = ':', color = "black")
+        ax.set_xlabel("Lightning Damage", color = "yellow")
+        ax.set_ylabel("Item Name", color = "green")
+        plt.grid(True)
+        st.pyplot(fig)
+elif page == "Fun facts":
+    if st.button("Let's count all weight weapons !"):
+        wei = ds3["Weight"].value_counts()
+        st.write(wei)
+    if st.button("Then , how much weight do we obtain if we add everything ?"):
+        weitotal = ds3["Weight"].sum()
+        st.write(f"Weight is {weitotal} kilos. Damn it's Really heavy !")
+
+
 
 
